198a17726f63dec895fc175fa57429ee48124f97
nilsreich <nilsreich@gmail.com>
2021-02-17 21:57:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <div class='wrapper'>
+    <button class='change__style' @click='changeStyle()'>Change Style</button>
+    <tabs :mode="mode">
+      <tab title="Tab 1">Hello From Tab 1</tab>
+      <tab title="Tab 2">Hello From Tab 2</tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+import Tab from "./components/Tab.vue";
+import Tabs from "./components/Tabs.vue";
+
+export default {
+  components: {
+    Tab,
+    Tabs,
+  },
+  data() {
+    return {
+      mode: "dark",
+    };
+  },
+  methods: {
+    changeStyle() {
+      if (this.mode === "dark") {
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    },
+  },
+};
+</script>
+
+<style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Karla", sans-serif;
+}
+.wrapper {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f8f8f8;
+  margin: 0;
+  padding: 20px;
+}
+
+.change__style {
+  background-color: #eee;
+  font-size: 1em;
+  margin-bottom: 10px;
+  padding: 5px;
+}
+</style>
